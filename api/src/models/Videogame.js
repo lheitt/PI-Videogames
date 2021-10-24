82cba132f96f3ae83c93afcd79dd8cf82615ecf6
lheitt <lheitt@gmail.com>
2021-10-24 04:03:48 -0300

    routes backend & db models created

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,31 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.STRING,
+    },
+    platforms: {
       type: DataTypes.STRING,
       allowNull: false,
     },
   });
 };
+
+
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,37 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
+const { Videogame, Genre } = require("../db")
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get("/videogames", async (req, res) =>{
+    const name = req.query.name;
+    const videogames = await Videogame.findAll();
+    res.send(videogames);
+});
+
+router.get("/videogames/:idVideogame", (req, res) =>{
+    const idVideogame = req.params.name;
+    res.send("soy GET /videogames/:idVideogame");
+});
+
+router.get("/genres", (req, res) =>{
+    res.send("soy GET /genres");
+});
+
+router.post("/videogame", async (req, res) => {
+    const {name, description, date, rating, platforms} = req.body;
+    const newGame = await Videogame.create({
+        name,
+        description,
+        date,
+        rating,
+        platforms
+    });
+    res.send(newGame);
+});
 
 module.exports = router;
